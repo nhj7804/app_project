55e13a246ecdc81654fac6b72fd257fe261f91c5
NOHYEONGJUN <n7804@naver.com>
2023-04-04 00:54:08 +0900

    [초기상태]

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class first extends StatelessWidget {
+  const first({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
